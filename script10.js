3fa75d5cd2342acd06ed109cff870fdce42d9ee7
Sh16coder <158461477+Sh16coder@users.noreply.github.com>
2025-03-14 14:53:48 +0530

    Create script10.js

---
+++ script10.js
@@ -0,0 +1,9 @@
+// Redirect to previous page
+function goBack() {
+    window.location.href = "notes.html";
+}
+
+// Open Google Drive folder in a new tab
+function openFolder(folderUrl) {
+    window.open(folderUrl, '_blank');
+}
