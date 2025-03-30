be94ab1ff6e78434c3df86122e1eaa1d6fbb815e
Sh16coder <158461477+Sh16coder@users.noreply.github.com>
2025-03-30 15:58:05 +0530

    Update gc.js

--- gc.js
+++ gc.js
@@ -1,12 +1,12 @@
 // Firebase configuration (replace with your own)
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-    projectId: "YOUR_PROJECT",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBQ2aQZdBhMk6-0hL1JOq05sJfVKtYfmhU",
+  authDomain: "group-chat-b2a3c.firebaseapp.com",
+  databaseURL: "https://group-chat-b2a3c-default-rtdb.firebaseio.com",
+  projectId: "group-chat-b2a3c",
+  storageBucket: "group-chat-b2a3c.firebasestorage.app",
+  messagingSenderId: "652138733251",
+  appId: "1:652138733251:web:c482f131b177a8b5f78c76"
 };
 
 // Initialize Firebase
